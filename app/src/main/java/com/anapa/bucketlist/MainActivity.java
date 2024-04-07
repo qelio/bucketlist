8bb91526a8314abaf3f3a32fc17304a4023c7ada
Azamat <96785836+Azamatvvvvvv@users.noreply.github.com>
2024-04-08 00:56:27 +0500

    амв

--- app/src/main/java/com/anapa/bucketlist/MainActivity.java
+++ app/src/main/java/com/anapa/bucketlist/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 int aaa;
+int f;
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
