17e530edd484106c39e4a16a7c16778990a85abd
Lehinskiy05 <peskovatskiy.leha@mail.ru>
2024-06-05 16:26:03 +0500

    serializable 1

--- app/src/main/java/com/anapa/bucketlist/Bucket.java
+++ app/src/main/java/com/anapa/bucketlist/Bucket.java
@@ -1,12 +1,24 @@
 package com.anapa.bucketlist;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Bucket {
+public class Bucket implements Serializable {
     public String Name;
     public String Description;
     public Boolean Achieved;
     public Date DeadLine;
     public List<Task> Tasks;
+
+    public Bucket() {}
+
+    public Bucket(String name, String description) {
+        Name = name;
+        Description = description;
+        Achieved = false;
+        DeadLine = new Date();
+        Tasks = new ArrayList<Task>();
+    }
 }
--- app/src/main/java/com/anapa/bucketlist/Task.java
+++ app/src/main/java/com/anapa/bucketlist/Task.java
@@ -1,6 +1,8 @@
 package com.anapa.bucketlist;
 
-public class Task {
+import java.io.Serializable;
+
+public class Task implements Serializable {
     public String Description;
     public Boolean Achieved;
 }
